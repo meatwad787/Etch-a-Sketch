5e28bf5f2abed922a6afd172297d01309d9bb16c
meatwad787 <greg_mrjohnson@yahoo.com>
2024-02-20 13:51:51 -0500

    Add code for a grid size based on user input
    I changed the original code for the first grid.
    Made it so that a row is added to the container
    and each box is added to that row.
    That repeats until the number is reached
    
    Also moved the event listeners out of the
    for loops for better performance.
    
    Removed the button creation and put it
    in the html file for convenience.
    
    Used `container.innerHTML = ''` to clear the
    baseGrid and make room for the new grid.

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 document.addEventListener("DOMContentLoaded", baseGrid());
 
 function baseGrid() {
+    let initGrid;
+    initGrid = 16;
     const container = document.querySelector("#container");
 
-    for (i = 0; i < 16 * 16; ++i) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        container.appendChild(square);
+    for (i = 0; i < initGrid; ++i) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        container.appendChild(row);
+        for (j = 0; j < initGrid; ++j) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            row.appendChild(square); 
+        }
     }
     const squares = document.querySelectorAll('.square');
-
     squares.forEach(square => {
         square.addEventListener('mouseenter', function() {
             this.classList.add('hovered'); 
@@ -18,4 +24,44 @@ function baseGrid() {
             this.classList.add('hovered');
         }); 
     });
-}
+
+}; 
+const btn = document.querySelector('.btn');
+//container.append(btn);
+
+function newGrid() {
+    
+    btn.addEventListener('click', function() {
+        
+        container.innerHTML = "";
+        createGrid();
+    });
+}; newGrid();
+
+function createGrid() {
+    let gridSize = prompt('How big of a grid?');
+   const container = document.querySelector("#container");
+   container.classList.add('container')
+   //container.appendChild(btn);
+
+ for (i = 0; i < gridSize; ++i) {
+    const newRow = document.createElement('div');
+    newRow.classList.add('row');
+    container.appendChild(newRow);
+       for (j = 0; j < gridSize; ++j) {
+       const newSquare = document.createElement('div');
+        newSquare.classList.add('square');
+        newRow.appendChild(newSquare);
+       }
+       const squares = document.querySelectorAll('.square');
+        squares.forEach(newSquare => {
+        newSquare.addEventListener('mouseenter', function() {
+            this.classList.add('hovered'); 
+        }); 
+        newSquare.addEventListener('mouseleave', function() {
+            this.classList.add('hovered');
+        }); 
+        });
+    }
+}; 
+
